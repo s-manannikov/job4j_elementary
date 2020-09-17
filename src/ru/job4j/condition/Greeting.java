8c9af47bb31b5edbfa7385b8711eaf7ecae238e5
s-manannikov <s_manannikov@mail.ru>
2020-09-17 17:13:19 +0300

    9.1. String [#361742]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main (String[] args) {
+        String idea = "I like Java!";
+        int year = 2020;
+        System.out.println(idea + " But I am a newbie. " + year);
+
+    }
+}
