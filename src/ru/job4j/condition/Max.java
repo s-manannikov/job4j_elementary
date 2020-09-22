868256dc7f2bbca734e4e02a2e2686bd8bde0c67
s-manannikov <s_manannikov@mail.ru>
2020-09-22 16:23:28 +0300

    4.5. Отладка программы в IDEA [#361679]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
         boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
+        return condition ? left : right;
     }
 
     public static void main(String[] args) {
